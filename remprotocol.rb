dc01a85bae806ba83aee3753028a235548ea84fa
Remme CI <vadym.kushnirenko@remme.io>
2019-10-11 09:23:20 -0700

    add changes

--- remprotocol.rb
+++ remprotocol.rb
@@ -1,9 +1,9 @@
 class Remprotocol < Formula
 
-   homepage "https://github.com/kushnirenko/remprotocol"
+   homepage "https://github.com/Remmeauth/remprotocol"
    revision 0
-   url "https://github.com/kushnirenko/remprotocol/archive/1.2.3.tar.gz"
-   version "1.2.3"
+   url "https://github.com/RemmeAuth/remprotocol/archive/1.2.6.tar.gz"
+   version "1.2.6"
 
    option :universal
 
@@ -11,12 +11,12 @@ class Remprotocol < Formula
    depends_on "gettext"
    depends_on "openssl"
    depends_on "libusb"
-   depends_on :macos => :high_sierra
+   depends_on :macos => :mojave
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/kushnirenko/remprotocol/releases/download/1.2.3"
-      sha256 "7dfdac355a9147614be6e6cf6f8595187a45f883516bdd4aed7d69048cafd2b4" => :mojave
+      root_url "https://github.com/RemmeAuth/remprotocol/releases/download/1.2.6"
+      sha256 "f668578390632323e01048998697a49375b75c66a469ef70c6c3f9152c8e2bcc" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
